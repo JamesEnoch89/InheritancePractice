efb331fcc35079a1c0ade67e8a5cc1cf946be507
jenoch <jenoch@JENOCH01>
2018-04-10 08:30:58 -0400

    totes forgot to turn this repo in

---
+++ InheritancePractice/IPet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePractice
+{
+    interface IPet
+    {
+        string OwnersName { get; set; }
+
+    }
+}
---
+++ InheritancePractice/Robot.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePractice
+{
+    class Robot : IPet
+    {
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public int VersionNumber { get; set; }
+        public string OwnersName { get; set; }
+        private bool IsAsleep { get; set; }
+        public Robot(string name)
+        {
+            Name = name;
+            Birthday = DateTime.Now;
+            IsAsleep = false;
+            int count = 0;
+            count++;
+            VersionNumber = count;
+        }
+
+        public void GetWakeState()
+        {
+            if (this.IsAsleep == true)
+            {
+                Console.WriteLine($"{this.Name} is asleep");
+            }else
+            {
+                this.SayHello();
+            }
+
+        }
+        public void StartUp()
+        {
+            IsAsleep = false;
+        }
+        public void ShutDown()
+        {
+            IsAsleep = true;
+        }
+        public void SayHello()
+        {
+            Console.WriteLine("I AM ROBOT");
+        }
+
+        public override string ToString()
+        {
+            return $"Robot: {Name} {Birthday} {VersionNumber}";
+        }
+    }
+}
+    }
+}
